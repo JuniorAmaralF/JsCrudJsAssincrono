8dd34c31192813f51ff5e1f62811811401daea9b
JuniorAmaralF <85956913+JuniorAmaralF@users.noreply.github.com>
2021-08-31 23:23:49 -0300

    Configurando api falsa

---
+++ admin/service/cliente-service.js
@@ -0,0 +1,10 @@
+const http = new XMLHttpRequest();
+
+http.open('GET','http://localhost:3000/profile');
+
+http.send();
+
+http.onload = function () {
+    const data = http.response;
+    console.log(data);
+}
