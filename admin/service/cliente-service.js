a3ba941a3751bc90e8decb590e481aba9e947cc8
JuniorAmaralF <85956913+JuniorAmaralF@users.noreply.github.com>
2021-09-02 23:15:02 -0300

    Refatorando responsabilidades

--- admin/service/cliente-service.js
+++ admin/service/cliente-service.js
@@ -1,24 +1,3 @@
-
-const criaNovaLinha = function(nome,email){
-    const linhaNovoCliente = document.createElement('tr');
-    const conteudo = `
-    <td class="td" data-td>${nome}</td>
-                <td>${email}</td>
-                <td>
-                    <ul class="tabela__botoes-controle">
-                        <li><a href="../telas/edita_cliente.html" class="botao-simples botao-simples--editar">Editar</a></li>
-                        <li><button class="botao-simples botao-simples--excluir" type="button">Excluir</button></li>
-                    </ul>
-                </td>
-                 `
-    
-    linhaNovoCliente.innerHTML = conteudo;
-
-    return linhaNovoCliente;
-
-}
-
-const tabela = document.querySelector('[data-tabela]');
 // ()=> arrow function
 const listaClientes = () =>{
     return fetch(`http://localhost:3000/profile`)
@@ -27,8 +6,6 @@ const listaClientes = () =>{
     })
 }
 
-listaClientes()
-.then(data => {
-    data.forEach(elemento =>{
-    tabela.appendChild(criaNovaLinha(elemento.nome,elemento.email));
-})})
+export const clienteService = {
+    listaClientes
+}
