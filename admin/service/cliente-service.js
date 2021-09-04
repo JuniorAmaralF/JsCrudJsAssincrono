8cc654ee9536913508b62c89d83b94356dd7d242
JuniorAmaralF <85956913+JuniorAmaralF@users.noreply.github.com>
2021-09-04 16:07:08 -0300

    Criando a funcao deleta um cliente

--- admin/service/cliente-service.js
+++ admin/service/cliente-service.js
@@ -24,9 +24,15 @@ const criaCliente = function(nome,email) {
         return resposta.body
     })
 }
-
+//recebe parametro id, que ira buscar no db.json na hora da exclusao
+const removeCliente = function (id){
+    return fetch('http://localhost:3000/profile/${id}',{
+        method:'DELETE'
+    })
+}
 
 export const clienteService = {
     listaClientes,
-    criaCliente
+    criaCliente,
+    removeCliente
 }
