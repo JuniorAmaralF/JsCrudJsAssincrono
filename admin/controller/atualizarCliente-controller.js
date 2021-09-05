a425df3424d4a0b315153752c9c494a4b0489190
JuniorAmaralF <85956913+JuniorAmaralF@users.noreply.github.com>
2021-09-05 10:05:21 -0300

    Async e Await

--- admin/controller/atualizarCliente-controller.js
+++ admin/controller/atualizarCliente-controller.js
@@ -1,25 +1,24 @@
 import { clienteService } from "../service/cliente-service.js";
 
-const pegaURL = new URL (window.location);
+(async function(){
+    const pegaURL = new URL (window.location);
 
-const id = pegaURL.searchParams.get('id');
-
-const inputNome = document.querySelector('[data-nome]');
-const inputEmail = document.querySelector('[data-email]');
-
-clienteService.detalhaCliente(id)
-.then(dados => {
+    const id = pegaURL.searchParams.get('id');
+    
+    const inputNome = document.querySelector('[data-nome]');
+    const inputEmail = document.querySelector('[data-email]');
+    
+    const dados = await clienteService.detalhaCliente(id)
     inputNome.value = dados.nome;
     inputEmail.value = dados.email;
-})
+    
+    const formulario = document.querySelector('[data-form]');
+    
+    formulario.addEventListener('submit', async function (evento) {
+        evento.preventDefault();
+    
+        await clienteService.atualizarCliente(id,inputNome.value,inputEmail.value)
+            window.location.href = "../telas/edicao_concluida.html"  
+    })
+})()
 
-const formulario = document.querySelector('[data-form]');
-
-formulario.addEventListener('submit', function (evento) {
-    evento.preventDefault();
-
-    clienteService.atualizarCliente(id,inputNome.value,inputEmail.value)
-    .then(()=>{
-        window.location.href = "../telas/edicao_concluida.html"
-    }) 
-})
